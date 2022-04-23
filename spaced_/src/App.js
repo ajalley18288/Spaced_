3f6d48a4d428d4a53e37865e7ffd977073d5d1fd
ajalley18288 <alyissasanders@my.unt.edu>
2022-04-23 15:48:35 -0500

    welcome screen start

--- spaced_/src/App.js
+++ spaced_/src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import background from "/Users/alyissasanders/Downloads/Spaced_/spaced_/src/image/HomeScreen.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <h1 className="text-9xl text-center border-2 min-h-full">Spaced_</h1>
   );
-}
+};
 
 export default App;
