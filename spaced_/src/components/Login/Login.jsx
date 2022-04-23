9e8b3e8efac31f51efe720a937a9ef9a75e59bfb
Max Celant <maxcelant@gmail.com>
2022-04-23 18:02:01 -0500

    login page almost done

--- spaced_/src/components/Login/Login.jsx
+++ spaced_/src/components/Login/Login.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import LoginImage from '../../assets/loginpageimage.jpg'
+import AuthContainer from './AuthContainer'
 
 const Login = () => {
   return (
       <div className='w-full h-screen relative'>
-          <h1 className="w-full h-full flex flex-col left-0 absolute text-white text-9xl font-bold justify-center items-center font-aquire">Spaced_</h1>
+          <h1 className="w-full h-full flex flex-col left-0 absolute text-white text-9xl font-bold items-center font-aquire mt-24">Spaced_</h1>
+          <div className="w-full h-full flex flex-col absolute justify-center items-center top-16 mt-10">
+            <AuthContainer/>
+          </div>
           <img src={LoginImage} className='h-full w-full object-fill'/>
       </div>
   )
