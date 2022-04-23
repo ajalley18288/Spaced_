b97afcde59de23689663d372d21d1a7454c57382
Max Celant <maxcelant@gmail.com>
2022-04-23 16:39:43 -0500

    reducers

--- spaced_/src/index.js
+++ spaced_/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { applyMiddleware, createStore, compose } from 'redux'
+import { Provider } from 'react-redux'
+import allReducers from './reducers';
+import thunk from 'redux-thunk'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+const store = createStore(allReducers, compose(applyMiddleware(thunk)))
+
+ReactDOM.render(
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>,
+  document.getElementById('root')
 );
---
+++ spaced_/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+const allReducers = combineReducers({
+    // userReducer
+    // taskReducer
+})
+
+export default allReducers
