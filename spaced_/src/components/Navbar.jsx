5a0c32010ff9b607fd3bd8152c8694be7340bfbc
malcolmmorton77 <malcolmmorton77@gmail.com>
2022-04-23 19:46:50 -0500

    navbar created and default framework added

--- spaced_/src/components/Navbar.jsx
+++ spaced_/src/components/Navbar.jsx
@@ -1,16 +1,120 @@
-import React from 'react'
+import React, { useState } from 'react'
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
 
 //all the svg files
 import logo from '../assets/logo.svg'
 import Home from '../assets/home-solid.svg'
-import Team from '../assets/social.svg'
 import Calender from '../assets/sceduled.svg'
 import Projects from '../assets/starred.svg'
 import Documents from '../assets/draft.svg'
 import PowerOff from '../assets/power-off-solid.svg'
-import styled from "styled-components";
 
+const Container = styled.div`
+  position: fixed;
+  .active {
+    border-right: 4px solid var(--white);
+    img {
+      filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
+        brightness(103%) contrast(103%);
+    }
+  }
+`;
 
+const SidebarContainer = styled.div`
+  background-color: var(--black);
+  width: 3.5rem;
+  height: 80vh;
+  margin-top: 1rem;
+  border-radius: 0 30px 30px 0;
+  padding: 1rem 0;
+  
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  position: relative;
+`;
+
+const SlickBar = styled.ul`
+  color: var(--white);
+  list-style: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: var(--black);
+  padding: 2rem 0;
+  position: absolute;
+  top: 6rem;
+  left: 0;
+  width: ${(props) => (props.clicked ? "12rem" : "3.5rem")};
+  transition: all 0.5s ease;
+  border-radius: 0 30px 30px 0;
+`;
+
+const Item = styled(NavLink)`
+  text-decoration: none;
+  color: var(--white);
+  width: 100%;
+  padding: 1rem 0;
+  cursor: pointer;
+  display: flex;
+  padding-left: 1rem;
+  &:hover {
+    border-right: 4px solid var(--white);
+    img {
+      filter: invert(100%) sepia(0%) saturate(0%) hue-rotate(93deg)
+        brightness(103%) contrast(103%);
+    }
+  }
+  img {
+    width: 1.2rem;
+    height: auto;
+    filter: invert(92%) sepia(4%) saturate(1033%) hue-rotate(169deg)
+      brightness(78%) contrast(85%);
+  }
+`;
+
+const Text = styled.span`
+  width: ${(props) => (props.clicked ? "100%" : "0")};
+  overflow: hidden;
+  margin-left: ${(props) => (props.clicked ? "1.5rem" : "0")};
+  transition: all 0.3s ease;
+`;
+
+const Logo = styled.div`
+  width: 2rem;
+  img {
+    width: 100%;
+    height: auto;
+  }
+`;
+
+const Profile = styled.div`
+  width: ${(props) => (props.clicked ? "14rem" : "3rem")};
+  height: 3rem;
+  padding: 0.5rem 1rem;
+  /* border: 2px solid var(--white); */
+  border-radius: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-left: ${(props) => (props.clicked ? "9rem" : "0")};
+  background-color: var(--black);
+  color: var(--white);
+  transition: all 0.3s ease;
+  img {
+    width: 2.5rem;
+    height: 2.5rem;
+    border-radius: 50%;
+    cursor: pointer;
+    &:hover {
+      border: 2px solid var(--grey);
+      padding: 2px;
+    }
+  }
+`;
 
 const Button = styled.button`
     background-color: var(--black);
@@ -25,6 +129,7 @@ const Button = styled.button`
     justify-content: center;
     align-items: center;
     position: relative;
+
     &::before,
     &::after {
       content: "";
@@ -42,56 +147,126 @@ const Button = styled.button`
       top: ${(props) => (props.clicked ? "1.2" : "1.5rem")};
       transform: ${(props) => (props.clicked ? "rotate(-135deg)" : "rotate(0)")};
     }
-  `;
+`;
+
+const Details = styled.div`
+  display: ${(props) => (props.clicked ? "flex" : "none")};
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const Name = styled.div`
+  padding: 0 1.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  h4 {
+    display: inline-block;
+  }
+  a {
+    font-size: 0.8rem;
+    text-decoration: none;
+    color: var(--grey);
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+`;
+
+const Logout = styled.button`
+  border: none;
+  width: 2rem;
+  height: 2rem;
+  background-color: transparent;
+  img {
+    width: 100%;
+    height: auto;
+    filter: invert(15%) sepia(70%) saturate(6573%) hue-rotate(2deg)
+      brightness(100%) contrast(126%);
+    transition: all 0.3s ease;
+    &:hover {
+      border: none;
+      padding: 0;
+      opacity: 0.5;
+    }
+  }
+`;
 
 const Navbar = () => {
+
+const [click, setClick] = useState(false);
+const handleClick = () => setClick(!click);
+
+const [profileClick, setprofileClick] = useState(false);
+  const handleProfileClick = () => setprofileClick(!profileClick);
+
     return (
-        <div className = "App">
-            <Button>Click Me</Button>
-            <div>
-                <div>
+        <Container className='z-10'>
+            <Button clicked={click} onClick={() => handleClick()}>
+                Click
+            </Button>
+            <SidebarContainer>
+                <Logo>
                     <img src={logo} alt="logo" />
-                </div>
-                <ul>
-                    <li>
-                        <img src="Home" alt="Home" />
-                        <span>Home</span>
-                    </li>
-                    <li>
-                        <img src="Team" alt="Team" />
-                        <span>Team</span>
-                    </li>
-                    <li>
-                        <img src="Calender" alt="Calender" />
-                        <span>Calender</span>
-                    </li>
-                    <li>
-                        <img src="Documents" alt="Documents" />
-                        <span>Documents</span>
-                    </li>
-                    <li>
-                        <img src="Projects" alt="Projects" />
-                        <span>Projects</span>
-                    </li>
-                </ul>
+                </Logo>
+                    <SlickBar clicked={click}>
+                    <Item
+                onClick={() => setClick(false)}
+                exact
+                activeClassName="active"
+                to="/"
+            >
+                <img src={Home} alt="Home" />
+                <Text clicked={click}>Home</Text>
+            </Item>
+            <Item
+                onClick={() => setClick(false)}
+                activeClassName="active"
+                to="/calender"
+            >
+                <img src={Calender} alt="Calender" />
+                <Text clicked={click}>Calender</Text>
+            </Item>
+            <Item
+                onClick={() => setClick(false)}
+                activeClassName="active"
+                to="/documents"
+            >
+                <img src={Documents} alt="Documents" />
+                <Text clicked={click}>Documents</Text>
+            </Item>
+            <Item
+                onClick={() => setClick(false)}
+                activeClassName="active"
+                to="/projects"
+            >
+                <img src={Projects} alt="Projects" />
+                <Text clicked={click}>Projects</Text>
+            </Item>
+                </SlickBar>
 
 
-                <div>
-                    <img src="https://picsum.photos/200" alt="Profile" />
-                    <div>
-                        <div>
-                            <h4>John Doe</h4>
-                            <a href="/#">view profile</a>
-                        </div>
+                <Profile clicked={profileClick}>
+                <img
+                    onClick={() => handleProfileClick()}
+                    src="https://picsum.photos/200"
+                    alt="Profile"
+                />
+                <Details clicked={profileClick}>
+                    <Name>
+                    <h4>John Doe</h4>
+                    <a href="/#">view profile</a>
+                    </Name>
 
-                            <button>
-                                <img src={PowerOff} alt="logout" />
-                            </button>
-                    </div>
-                </div>
-            </div>
-        </div>
-    )
-}
+                    <Logout>
+                        <img src={PowerOff} alt="logout" />
+                    </Logout>
+                </Details>
+                </Profile>
+            </SidebarContainer>
+        </Container>
+    );
+};
 
-export default Navbar
+export default Navbar;
